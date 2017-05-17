304692ccb7a3ed081e52b8f032f00a98eb7f985c
Francisco Oliveira <delley.fx@gmail.com>
2017-05-16 21:38:28 -0300

    Add index.html

--- src/hello-html.js
+++ src/hello-html.js
@@ -1,9 +1,11 @@
-var http = require('http');
+var http = require('http')
+  , fs = require('fs')
+  , index = fs.readFileSync('index.html');
 
 http.createServer(function(request, response){
   response.writeHead(200, {'Content-type':'text/html'});
-  response.write('<h1>Be MEAN</h1>');
-  response.end();
+  // response.write('<h1>Be MEAN</h1>');
+  response.end(index);
 }).listen(3000, function(){
   console.log('Servidor rodando em localhost:3000');
 });
