99ee1790c75981a18dd461f136fee1b12fffc42a
Francisco Oliveira <delley.fx@gmail.com>
2017-05-20 20:36:44 -0300

    add cabeçalho h1 e h2

--- src/hello-querystring.js
+++ src/hello-querystring.js
@@ -1,4 +1,6 @@
-var http = require('http')
+'use strict'
+
+let http = require('http')
   , url = require('url');
 
 http.createServer(function(request, response){
@@ -7,7 +9,8 @@ http.createServer(function(request, response){
 
   response.writeHead(200, {'Content-type':'text/html'});
   response.write('<html><body>');
-  response.write('<h1>Query String</h1>');
+  response.write('<h1>Be - MEAN</h1>');
+  response.write('<h2>Query String</h2>');
   response.write('<ul>');
 
   for(var key in result.query) {
