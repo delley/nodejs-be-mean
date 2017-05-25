ae0ad0d7270a21df459ce293015376a6298608c0
Francisco Oliveira <delley.fx@gmail.com>
2017-05-24 21:37:59 -0300

    fix código reposta questão 5

--- exercicios/class-02/server.js
+++ exercicios/class-02/server.js
@@ -12,24 +12,24 @@ const http = require('http')
       }
     ;
 
-  http.createServer(function(request, response){
+http.createServer(function(request, response){
 
-    var url = request.url;
+  var url = request.url;
 
-    switch(url) {
-      case '/api/create':
-      case '/api/read':
-      case '/api/update':
-      case '/api/delete':
-        SUCCESS.rota = url;
-        response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8'});
-        response.write(JSON.stringify(SUCCESS));
-        break;
-      default:
-        response.writeHead(404, { 'Content-Type': 'application/json; charset=utf-8'});
-        response.write(JSON.stringify(ERROR));
-    }
-    response.end();
-  }).listen(3000, function(){
-    console.log('Servidor rodando em localhost:3000');
-  });
+  switch(url) {
+    case '/api/create':
+    case '/api/read':
+    case '/api/update':
+    case '/api/delete':
+      SUCCESS.rota = url;
+      response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8'});
+      response.write(JSON.stringify(SUCCESS));
+      break;
+    default:
+      response.writeHead(404, { 'Content-Type': 'application/json; charset=utf-8'});
+      response.write(JSON.stringify(ERROR));
+  }
+  response.end();
+}).listen(3000, function(){
+  console.log('Servidor rodando em localhost:3000');
+});
